3ad645d2652eab2d3c677a83fcf33176f02d1204
MariannaLiss <mlisitsyna88@gmail.com>
2022-11-10 00:55:14 +0300

    testOpenAndCheckTheMenuGuide() and testOpenAndCheckTheTemperatureInFahrenheit() added

--- src/test/java/MariannaLissTest.java
+++ src/test/java/MariannaLissTest.java
@@ -5,12 +5,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-
 public class MariannaLissTest {
-
     /*
-    TC_1_1  - Тест кейс:
     1. Открыть страницу https://openweathermap.org/
     2. Набрать в строке поиска город Paris
     3. Нажать пункт меню Search
@@ -28,7 +24,7 @@ public class MariannaLissTest {
         String expectedResult = "Paris, FR";
 
         driver.get(url);
-        Thread.sleep(5000);
+        Thread.sleep(7000);
 
         WebElement searchCityField = driver.findElement(
                 By.xpath("//div[@id = 'weather-widget']//input[@placeholder = 'Search city']")
@@ -40,8 +36,7 @@ public class MariannaLissTest {
                 By.xpath("//div[@id = 'weather-widget']//button[@type = 'submit']")
         );
         searchButton.click();
-
-        Thread.sleep(1000);
+        Thread.sleep(7000);
 
         WebElement parisFRChoiceInDropdownMenu = driver.findElement(
                 By.xpath("//ul[@class = 'search-dropdown-menu']/li/span[text() = 'Paris, FR ']")
@@ -51,10 +46,72 @@ public class MariannaLissTest {
         WebElement h2CityCountryHeader = driver.findElement(
                 By.xpath("//div[@id = 'weather-widget']//h2")
         );
-        Thread.sleep(2000);
+        Thread.sleep(7000);
         String actualResult = h2CityCountryHeader.getText();
 
         Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
+    /*
+    1. Открыть базовую ссылку
+    2. Нажать на пункт меню Guide
+    3. Подтвердить, что вы перешли на страницу со ссылкой https://openweathermap.org/guide
+    и что title этой страницы OpenWeatherMap API guide - OpenWeatherMap
+     */
+
+    @Test
+    public void testOpenAndCheckTheMenuGuide() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/79213/ChromeDrivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        String url = "https://openweathermap.org/";
+        String expectedResult1 = "https://openweathermap.org/guide";
+        String expectedResult2 = "OpenWeatherMap API guide - OpenWeatherMap";
+
+        driver.get(url);
+        Thread.sleep(7000);
+        WebElement guideElementInMenu = driver.findElement(
+                By.xpath("//a[@href='/guide']")
+        );
+        guideElementInMenu.click();
+        String actualResult2 = driver.getTitle();
+        String actualResult1 = driver.getCurrentUrl();
+
+        Assert.assertEquals(actualResult1, expectedResult1);
+        Assert.assertEquals(actualResult2, expectedResult2);
+
+        driver.quit();
+    }
+
+    /*
+    1. Открыть базовую ссылку
+    2. Нажать на единицы измерения
+    3. Подтвердить, что температура для города показана в Фаренгейтах
+    */
+
+    @Test
+    public void testOpenAndCheckTheTemperatureInFahrenheit() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "C:/Users/79213/ChromeDrivers/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        String url = "https://openweathermap.org/";
+        String fTempSymbol = "°F";
+
+        driver.get(url);
+        driver.manage().window().maximize();
+        Thread.sleep(5000);
+        WebElement menuImperial = driver.findElement(
+                By.xpath("//div[@class = 'switch-container']/div[@class='option']/following-sibling::div")
+        );
+        menuImperial.click();
+        Thread.sleep(2000);
+        WebElement tempF = driver.findElement(
+                By.xpath("//div[@class='current-temp']/span")
+        );
+        String tempInF = tempF.getText();
+
+        Assert.assertTrue(tempInF.contains(fTempSymbol));
 
         driver.quit();
     }
